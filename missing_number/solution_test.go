59242be452463bcf1d41d48b6147660e3571ff21
gleb.nazemnov <gleb.nazemnov@aliexpress.ru>
2022-04-28 20:34:58 +0200

    Missing Number

---
+++ missing_number/solution_test.go
@@ -0,0 +1,43 @@
+package missing_number
+
+import "testing"
+
+func Test_missingNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "example 1",
+			args: args{
+				nums: []int{0, 1, 3},
+			},
+			want: 2,
+		},
+		{
+			name: "example 2",
+			args: args{
+				nums: []int{9, 6, 4, 2, 3, 5, 7, 0, 1},
+			},
+			want: 8,
+		},
+		{
+			name: "example 3",
+			args: args{
+				nums: []int{0, 1},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := missingNumber(tt.args.nums); got != tt.want {
+				t.Errorf("missingNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
