a4f55e68cdbec2745ba05937665996355a04fba3
glebnaz <nazemnovgleb@gmail.com>
2022-06-08 14:24:21 +0200

    max dept of btree

---
+++ solutions/maximum_depth_of_binary_tree/solution.go
@@ -0,0 +1,57 @@
+package maximum_depth_of_binary_tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	max := -1
+
+	var stack []*TreeNode
+	isInter := make(map[*TreeNode]struct{})
+	lvl := 1
+
+	for root != nil || len(stack) > 0 {
+		isInter[root] = struct{}{}
+
+		stack = append(stack, root)
+
+		if root.Left != nil {
+			if _, ok := isInter[root.Left]; !ok {
+				root = root.Left
+				lvl++
+				continue
+			}
+		}
+
+		if root.Right != nil {
+			if _, ok := isInter[root.Right]; !ok {
+				root = root.Right
+				lvl++
+				continue
+			}
+		}
+
+		if root.Left == nil && root.Right == nil {
+			if max < lvl {
+				max = lvl
+			}
+		}
+
+		if len(stack)-2 < 0 {
+			break
+		}
+
+		root = stack[len(stack)-2]
+		stack = stack[0 : len(stack)-2]
+		lvl--
+	}
+
+	return max
+}
